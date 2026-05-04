9d2b9f33cbe2c3828d383dd8a0197f72a406381f
Donal <ocallaghandonal2@gmail.com>
2026-05-04 18:29:21 +0100

    Fix LinkedIn URL to donalocallaghan1

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,6 @@
 export const social = {
   email: "hello@donalocallaghan.com",
   github: "https://github.com/donalocallaghan",
-  linkedin: "https://www.linkedin.com/in/donalocallaghan/",
+  linkedin: "https://www.linkedin.com/in/donalocallaghan1/",
   x: "https://x.com/donalocallaghan",
 };
